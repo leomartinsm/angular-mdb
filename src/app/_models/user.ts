3b6c94ae3c8ddde220d54794df436d0eb8b2db61
unknown <leonardo.martins.medeiros@live.com>
2020-05-29 13:50:33 -0300

    inicio

---
+++ src/app/_models/user.ts
@@ -0,0 +1,11 @@
+﻿import { Role } from "./role";
+
+    export class User {
+    id: number;
+    racf: string;
+    password: string;
+    nome: string;
+    funcional: string;
+    role: Role;
+    token?: string;
+}
